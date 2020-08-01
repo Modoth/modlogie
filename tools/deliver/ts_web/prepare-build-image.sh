6fbaf0b5d0938a7d6f688895eb1ddc7eff55ba95
z_xueqin <z_xueqin@icloud.com>
2020-08-01 05:07:20 +0000

    fix blog print style

--- tools/deliver/ts_web/prepare-build-image.sh
+++ tools/deliver/ts_web/prepare-build-image.sh
@@ -2,10 +2,11 @@
 set -o errexit
 
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-src=`realpath "$scripts_dir/../../../client"`
-build_dir=`realpath "$scripts_dir/../build/client"`
+src=`realpath "$scripts_dir/../../../clients/ts_web"`
+build_dir="$scripts_dir/../build/ts_web"
 rm -rf "$build_dir" || true
 mkdir -p "$build_dir"
+build_dir=`realpath "$build_dir"`
 target_dir="$build_dir/app"
 ( cd "$src" && target_dir="$target_dir" npm run build-to/client-prod )
 cp -rf "$scripts_dir/docker" -T "$build_dir/docker"
