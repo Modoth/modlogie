e676f77f72b8dcf9a6d9239669299dad16f3d877
z_xueqin <z_xueqin@icloud.com>
2020-11-04 19:17:08 +0000

    H5 storages

--- clients/ts_web/src/plugins/base/common/Hightlight.tsx
+++ clients/ts_web/src/plugins/base/common/Hightlight.tsx
@@ -1,17 +1,29 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useState } from "react";
 import { Prism } from "react-syntax-highlighter";
 import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { Slider } from 'antd';
+import './Hightlight.less'
 
 export default function Highlight(props: {
     value: string,
     language: string
 }) {
-
+    const [store] = useState({ fontSize: 1 })
     const { language, value } = props;
+    const ref = React.createRef<HTMLDivElement>()
     return (
-        <Prism language={language} style={coy}>
-            {value}
-        </Prism>
+        <div className="hightlight" ref={ref} style={{ fontSize: `${store.fontSize}em` }}>
+            <Slider vertical={true} tooltipVisible={false} className="font-selector" onChange={value => {
+                store.fontSize = typeof (value) === 'number' ? value : value[0]
+                if (ref.current) {
+                    ref.current.style.fontSize = `${store.fontSize}em`
+                }
+            }} defaultValue={store.fontSize} min={0.6} max={1} step={0.1} />
+            <Prism wrapLines={true} language={language} style={coy}>
+                {value}
+            </Prism>
+        </div>
+
     );
 
 }
