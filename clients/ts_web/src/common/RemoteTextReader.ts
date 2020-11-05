ca81c35b81ec359d8b726eb880caece1f44e1e8f
z_xueqin <z_xueqin@icloud.com>
2020-11-05 14:07:47 +0000

    download

---
+++ clients/ts_web/src/common/RemoteTextReader.ts
@@ -0,0 +1,23 @@
+import { fetchFileRange, fetchFileSize } from "./FetchUtils";
+
+export class RemoteTextReader {
+    constructor(private url: string, private label = 'utf-8', private buffSize = 256) { }
+    private _size: Promise<number>
+    private fetchedSize = 0
+    private decoder = new TextDecoder(this.label)
+    get size(): Promise<number> {
+        if (!this._size) {
+            this._size = fetchFileSize(this.url)
+        }
+        return this._size;
+    }
+    async read(): Promise<[string, boolean]> {
+        const size = await this.size
+        if (this.fetchedSize >= size) {
+            return [this.decoder.decode(), true]
+        }
+        const buff = await fetchFileRange(this.url, this.fetchedSize, Math.min(this.fetchedSize + this.buffSize, size))
+        this.fetchedSize += buff.byteLength
+        return [this.decoder.decode(buff, { stream: true }), this.fetchedSize >= size]
+    }
+}
--- clients/ts_web/src/plugins/resfile/view/ResFileViewers/ResPlain.tsx
+++ clients/ts_web/src/plugins/resfile/view/ResFileViewers/ResPlain.tsx
@@ -1,43 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { ResFileViewerProps } from './ResFileViewerProps'
 import './ResPlain.less'
-import { fetchFileRange, fetchFileSize } from './common';
-const blockSize = 256;
+import { RemoteTextReader } from '../../../../common/RemoteTextReader';
 
 export function ResPlain(props: ResFileViewerProps) {
-    const [size, setSize] = useState(0)
-    const [fetchedSize, setFetchedSize] = useState(0)
-    const [lastBuff, setLastBuff] = useState<ArrayBuffer | undefined>()
-    const [decoder] = useState(new TextDecoder('utf-8'));
-    const [lastTxt, setLastTxt] = useState(props.buff ? decoder.decode(new Uint8Array(props.buff, 0, blockSize), { stream: true }) : '')
-    const ref = React.createRef<HTMLDivElement>()
+    const blockSize = 256;
+    const encoding = 'utf-8'
+    const [reader] = useState(new RemoteTextReader(props.url, encoding, blockSize))
+    const [lastTxt, setLastTxt] = useState(props.buff ? new TextDecoder(encoding).decode(new Uint8Array(props.buff, 0, blockSize), { stream: true }) : '')
     useEffect(() => {
         if (!props.buff) {
-            fetchFileSize(props.url).then(setSize)
+            fetchMore()
         }
     }, [])
-    useEffect(() => {
-        if (!size || props.buff) {
-            return
-        }
-        fetchMore()
-    }, [size])
-    useEffect(() => {
-        if (!ref.current) {
-            return
-        }
-        console.log(ref.current)
-        console.log(ref.current.innerText.length)
-    })
     const fetchMore = async () => {
-        if (fetchedSize >= size) {
-            decoder.decode()
-            return
-        }
-        const buff = await fetchFileRange(props.url, fetchedSize, Math.min(fetchedSize + blockSize, size))
-        setFetchedSize(buff.byteLength + fetchedSize)
-        setLastBuff(buff)
-        setLastTxt(decoder.decode(buff, { stream: true }))
+        const [txt] = await reader.read()
+        setLastTxt(txt)
     }
     return <div className="resplain">
         <div className="content"> {lastTxt}</div>
