736bfdd25a2d1a13d756b95aeb603e36d4147a24
z_xueqin <z_xueqin@icloud.com>
2020-11-10 03:04:39 +0000

    sperate deploy

--- tools/deliver/ts_web/build-image.sh
+++ tools/deliver/ts_web/build-image.sh
@@ -5,6 +5,7 @@ if [ -z "$IMAGE_USER" ]; then exit 1; fi
 if [ -z "$IMAGE_VERSION" ]; then exit 1; fi
 if [ -n "$BUILD_SERVER" ]; then  export DOCKER_HOST="ssh://$BUILD_SERVER"; fi
 
+export API_BASE="$app_api_base"
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 build_dir="$scripts_dir/../build/ts_web"
 cd "$build_dir"
