5d24d00283f111f7a218e5c079b1cb8d377b3205
z_xueqin <z_xueqin@icloud.com>
2020-12-05 13:55:36 +0000

    publish

--- clients/ts_web/src/plugins/blog/view/PlainGenerator.tsx
+++ clients/ts_web/src/plugins/blog/view/PlainGenerator.tsx
@@ -5,20 +5,16 @@ import ReactDOMServer from 'react-dom/server'
 import ReactMarkdown from 'react-markdown'
 import renderers from './renderers'
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
-import PlainStyle from '!!raw-loader!./PlainStyle.css'
-
 const PlainGenerator = (props:ArticleContentViewerProps) => {
   const sections = (props.content.sections || []).filter(s => s.content)
-  return ReactDOMServer.renderToStaticMarkup(<div>
-    {PlainStyle ? <style>{PlainStyle}</style> : undefined}
+  return ReactDOMServer.renderToStaticMarkup(<article>
     {
-      sections.map((s, i) => <div key={s.name!} className={classNames(s.name, `section-${i}`, props.type?.additionalSections?.has(s.name!) ? 'addition' : 'normal')}>
+      sections.map((s, i) => <section key={s.name!} className={classNames(s.name, `section-${i}`, props.type?.additionalSections?.has(s.name!) ? 'addition' : 'normal')}>
         {i ? <h3 className="section-title">{s.name}</h3> : undefined }
-        <div className="section-content"><ReactMarkdown renderers={renderers} source={s.content} skipHtml={true}></ReactMarkdown></div>
-      </div>)
+        <section className="section-content"><ReactMarkdown renderers={renderers} source={s.content} skipHtml={true}></ReactMarkdown></section>
+      </section>)
     }
-  </div>)
+  </article>)
 }
 
 export default PlainGenerator
