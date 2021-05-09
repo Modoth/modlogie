5a737d621357294d36c0d235f729c1593e841714
z_xueqin <z_xueqin@icloud.com>
2021-05-09 05:00:00 +0000

    auto

--- clients/ts_web/src/view/pages/ArticleList.tsx
+++ clients/ts_web/src/view/pages/ArticleList.tsx
@@ -21,7 +21,7 @@ const ColumnCountKey = 'ARTICLES_COLUMN_COUNT'
 
 const BorderStyleKey = 'ARTICLES_BORDER_STYLE'
 
-const ShowIndexKey = 'ARTICLES_SHOW_INDEX'
+const HideIndexKey = 'ARTICLES_HIDE_INDEX'
 
 export default function ArticleList () {
   const locate = useServicesLocate()
@@ -71,15 +71,15 @@ export default function ArticleList () {
   const smallScreen = window.matchMedia && window.matchMedia('(max-width: 780px)')?.matches
   const [columnCount, setColumnCount] = useState(1)
   const [borderStyle, setBorderStyle] = useState(0)
-  const [showIdx, setShowIdx] = useState(false)
+  const [hideIdx, setHideIdx] = useState(false)
   const configsService = locate(IUserConfigsService)
   const loadConfigs = async () => {
     const columnCount = await configsService.getOrDefault(ColumnCountKey, (smallScreen ? 1 : 2))
     const borderStyle = await configsService.getOrDefault(BorderStyleKey, 0)
-    const showIdx = await configsService.getOrDefault(ShowIndexKey, false)
+    const hideIdx = await configsService.getOrDefault(HideIndexKey, false)
     setColumnCount(columnCount)
     setBorderStyle(borderStyle)
-    setShowIdx(showIdx)
+    setHideIdx(hideIdx)
   }
   useEffect(() => {
     loadConfigs()
@@ -103,9 +103,9 @@ export default function ArticleList () {
         <Button type="link" size="large" icon={<ArrowLeftOutlined />} onClick={close} />
         <span className="spilter"></span>
         <Button type="link" size="large" icon={<OrderedListOutlined />} onClick={() => {
-          const next = !showIdx
-          setShowIdx(next)
-          configsService.set(ShowIndexKey, next)
+          const next = !hideIdx
+          setHideIdx(next)
+          configsService.set(HideIndexKey, next)
         }} />
         <Button type="link" size="large" icon={<PicRightOutlined />} onClick={() => {
           const next = ((columnCount) % maxColumn) + 1
@@ -125,7 +125,7 @@ export default function ArticleList () {
         }
         <Button type="link" size="large" icon={<PrinterOutlined />} onClick={() => window.print()} />
       </div>
-      <div ref={ref} className={classNames(`column-count-${columnCount}`, showIdx ? 'show-idx' : '', `border-style-${borderStyle}`, 'article-list')}>{items.filter(([article]) => article.content && article.content.sections).map(
+      <div ref={ref} className={classNames(`column-count-${columnCount}`, !hideIdx ? 'show-idx' : '', `border-style-${borderStyle}`, 'article-list')}>{items.filter(([article]) => article.content && article.content.sections).map(
         ([article, type]) =>
           article.lazyLoading ? <div key={article.name + '_ept'}></div>
             : <type.Viewer articleId={article.id!} title={article.name} key={article.name} showTitle={!type.noTitle} print={true} className={classNames("article", borderStyle==4 ? generateRandomStyle() : '')} content={article.content!} files={article.files} type={type}></type.Viewer>
